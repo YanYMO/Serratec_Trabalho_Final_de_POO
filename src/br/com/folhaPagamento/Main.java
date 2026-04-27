3dee45face0095456fc380abcf7d0c1c254968c5
YanYMO <yanmartinsd3@gmail.com>
2026-04-27 17:34:55 -0300

    Ajustes pedidos pelo professor, após correção.

---
+++ src/br/com/folhaPagamento/Main.java
@@ -0,0 +1,90 @@
+package br.com.folhaPagamento;
+
+import br.com.folhaPagamento.dao.DependenteDAO;
+import br.com.folhaPagamento.dao.FolhaPagamentoDAO;
+import br.com.folhaPagamento.dao.FuncionarioDAO;
+import br.com.folhaPagamento.infra.ConnectionFactory;
+import br.com.folhaPagamento.model.Contracheque;
+import br.com.folhaPagamento.model.Dependente;
+import br.com.folhaPagamento.model.FolhaPagamento;
+import br.com.folhaPagamento.model.Funcionario;
+import br.com.folhaPagamento.service.Arquivo;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        //Cria uma conexão com o Banco de Dados que será reutilizada durante a execução.
+        Connection connection = new ConnectionFactory().getConnection();
+        //Cria os objetos de cada Classe DAO.
+        FuncionarioDAO dao = new FuncionarioDAO(connection);
+        DependenteDAO dao2 = new DependenteDAO(connection);
+        FolhaPagamentoDAO dao3 = new FolhaPagamentoDAO(connection);
+
+        //Cria uma instancia de arquivo que será usada para leitura e escrita.
+        Arquivo arquivo = new Arquivo();
+
+        //Menu inicial do programa.
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("               Sistema de calculo de Folha de Pagamento              ");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("Para calcular a folha de pagamento, informe o local, nome e extenção\n" +
+                           "do arquivo que contenha os dados dos funcionários e seus dependentes.");
+
+        //While para repetir a solicitação ao usuário caso o arquivo não seja encontrado.
+        List<Funcionario> funcionarios = null;
+        while (funcionarios == null) {
+            System.out.print("Arquivo = ");
+            String arquivoEntrada = scan.nextLine();
+            try {
+                funcionarios = arquivo.lerArquivo(arquivoEntrada);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage() + ", tente novamente.");
+            }
+        }
+
+        // Adiciona os dados validados ao banco de dados.
+        try {
+            //Faz a inserção das informações ao percorrer as
+            //listas de br.com.FolhaPagamento.model.Funcionario, br.com.FolhaPagamento.model.Dependente e br.com.FolhaPagamento.model.FolhaPagamento.
+            for (Funcionario fun : funcionarios) {
+                dao.inserir(fun);
+
+                for (Dependente den : fun.getDependente()) {
+                    dao2.inserir(fun, den);
+                }
+                for (FolhaPagamento fol : fun.getFolhaPagamento()) {
+                    dao3.inserir(fun, fol);
+                }
+            }
+        } catch (RuntimeException e) {
+            System.out.println("Na inserção de dados no Banco de Dados houve um " + e.getMessage());
+        }
+        System.out.println("\nLeitura do arquivo concluída!");
+        System.out.println("Dados inseridos no Banco de Dados!\n");
+
+        //Solicita ao usuário um nome para o arquivo .csv que será exportado e realiza a escrita.
+        //Não é necessário colocar a extenção do arquivo.
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("Digite o nome do arquivo da folha de pagamento.");
+        System.out.print("Arquivo = ");
+        String arquivoSaida = scan.nextLine();
+        System.out.println();
+
+        List<Contracheque> contracheque = dao3.listar();
+        arquivo.escreverArquivo(arquivoSaida, contracheque);
+
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Erro ao fechar conexão com o Banco de Dados: " + e.getMessage());
+        }
+    }
+}
